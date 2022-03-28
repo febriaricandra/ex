2cf6c31b3346aae5eeb30204a47301fca4b4e61c
febriaricandra <febriaricandraproduction@gmail.com>
2022-03-28 11:11:24 +0700

    entry point

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const req = require('express/lib/request')
+const res = require('express/lib/response')
+const app = express()
+
+
+app.get('/', (req,res) =>{
+    res.send('hello, world')
+} )
+
+
+const PORT = 5000
+
+app.listen(PORT, ()=>{
+    console.log(`starting development server: http://localhost:${PORT}`)
+})
