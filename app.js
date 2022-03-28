358116ab591bfa0c380ba220db60ffa004d74f60
febriaricandra <febriaricandraproduction@gmail.com>
2022-03-28 12:09:39 +0700

    templating dengan ejs

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ app.set('view engine', 'ejs')
 
 require('./src/routes/home.routes')(app)
 
+app.use(express.static('public'))
+app.use('/css', express.static(__dirname + 'public/css'))
+app.use('/js', express.static(__dirname + 'public/js'))
+app.use('/img', express.static(__dirname + 'public/img'))
 
 const PORT = 5000
 
