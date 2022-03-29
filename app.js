5f12bb0799e8a356ebf4f5436ba61614a7c8adfe
febriaricandra <febriaricandraproduction@gmail.com>
2022-03-29 15:32:09 +0700

    add method POST and urlencoded false

--- app.js
+++ app.js
@@ -1,19 +1,28 @@
+//load express
 const express = require('express')
 const req = require('express/lib/request')
 const res = require('express/lib/response')
 const app = express()
 
+// set file
 app.set('views', './src/views')
 app.set('view engine', 'ejs')
 
+// register routes
 require('./src/routes/home.routes')(app)
 require('./src/routes/blog.routes')(app)
 
+// register public static
+app.use(express.urlencoded({
+    extended: false
+}))
 app.use(express.static('public'))
 app.use('/css', express.static(__dirname + 'public/css'))
 app.use('/js', express.static(__dirname + 'public/js'))
 app.use('/img', express.static(__dirname + 'public/img'))
 
+
+// add port localhost
 const PORT = 5000
 
 app.listen(PORT, ()=>{
--- src/routes/blog.routes.js
+++ src/routes/blog.routes.js
@@ -1,6 +1,9 @@
 module.exports = (app) => {
     const router = require('express').Router()
     const blog = require('../controllers/blog.controller')
+
+    //method
     router.get('/create', blog.create)
+    router.post('/', blog.store)
     app.use('/blog', router)
 }
